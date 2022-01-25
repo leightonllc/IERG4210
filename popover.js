4bcefc34ef825f6dca0dc849eb818545851e166b
Leighton Lau <92159440+leightonllc@users.noreply.github.com>
2022-01-25 16:16:03 +0800

    Create popover.js

---
+++ popover.js
@@ -0,0 +1,13 @@
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    const popoverId = popoverTriggerEl.attributes['data-content-id'];
+    if (popoverId) {
+        const contentEl=$(`#${popoverId.value}`).html();
+        return new bootstrap.Popover(popoverTriggerEl, {
+            content: contentEl,
+            html: true,
+	    sanitize: false,
+        });
+    }else{//do something else cause data-content-id isn't there.
+    }
+})
